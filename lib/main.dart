cc1ddfaacc01f2595f9048bbf08eed183e750225
inch <sxin2949@naver.com>
2022-04-23 00:09:50 +0900

    플러터 프로젝트 폴더와 기본 코드 이해하기 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyInchApp());
+void main() => runApp(const MyInchApp());
 
 class MyInchApp extends StatelessWidget {
+  const MyInchApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '인치 앱',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var center = Center(
+      child: Column(
+        children: const <Widget>[
+          Text('Hello1'),
+          Text('Hello'),
+          Text('Hello'),
+        ],
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('인치 앱'),
+      ),
+      body: center,
     );
   }
 }
