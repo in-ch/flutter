5c031c3a830008ca4b4ad690d15826393668a22c
inch <sxin2949@naver.com>
2022-04-25 23:02:37 +0900

    13 클래스와 위젯의 정체2

--- lib/12.dart
+++ lib/12.dart
@@ -1,11 +1,28 @@
+// ignore_for_file: avoid_print, unnecessary_new, file_names
+
 class Person {
-  String name = 'Alek';
+  late String name;
   late int age;
-  late String sex;
+  late String sex; // 멤버 변수라고 부름.
+
+  Person({String? name, int? age, String? sex}) {
+    // 이렇게 {}로 묶으면 named argument가 됨.
+    // 생성자라고 부름(인스턴스가 만들어 질 때 딱 한 번만 호출됨.)
+    this.name = name!;
+    this.age = age!;
+    this.sex = sex!;
+  }
+}
+
+int addNumber(int num1, int num2) {
+  return num1 + num2;
 }
 
 void main() {
-  Person p1 = new Person();
-  p1.age = 30;
+  Person p1 = new Person(age: 30);
+  Person p2 = new Person(sex: 'male');
+
   print(p1.age);
+  print(p2.age);
+  addNumber(3, 4);
 }
