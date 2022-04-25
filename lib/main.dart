7b91ca045c639121ac07f79fa4856911614e6f0e
inch <sxin2949@naver.com>
2022-04-25 23:13:31 +0900

    메뉴 아이콘 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -9,111 +9,44 @@ class MyInchApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BBANTO',
-      home: Grade(),
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text('BBANTO'),
-        backgroundColor: Colors.amber[700],
+        title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thor.png'),
-                radius: 80.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            Text('NAME',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                )),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('BBANTO',
-                style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text('BBANTO POWER LEVEL',
-                style: TextStyle(color: Colors.white, letterSpacing: 2.0)),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-                style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(width: 10.0),
-                Text('using lightasber',
-                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
-              ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(width: 10.0),
-                Text('face hero tattoo',
-                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
-              ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(width: 10.0),
-                Text('fire flames',
-                    style: TextStyle(fontSize: 16.0, letterSpacing: 1.0)),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thor_hammar.png'),
-                radius: 30.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('menu button is clicked');
+          },
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search button is clicked');
+            },
+          ),
+        ],
       ),
     );
   }
